e33ad5c4113cca82875a613723136bb212c69899
L1C <43138541+Jim78778@users.noreply.github.com>
2026-01-17 18:54:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+set -e
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+DIYPATH="package/diypath"
+OPENWRT_DIR="$(pwd)"
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+mkdir -p $DIYPATH
 
-# 跳过 mx4200 DTS，避免 patch 失败
-sed -i '/ipq8174-mx4200.dtsi/d' \
-target/linux/qualcommax/patches-6.6/*.patch
+echo "=== Step 1: Add QModem ==="
+# 使用你 fork 的 QModem 仓库
+git clone --depth=1 https://github.com/FUjr/QModem.git $DIYPATH/qmodem
+
+# 修复 QModem Makefile 中不必要的依赖
+sed -i \
+  -e 's/kmod-mhi-wwan/kmod-usb-net-qmi-wwan/g' \
+  -e 's/quectel-CM-5G//g' \
+  -e 's/quectel-cm//g' \
+  $DIYPATH/qmodem/application/qmodem/Makefile
+
+echo "=== Step 2: Add AdGuardHome ==="
+# 直接从官方 feeds 拉 luci-app-adguardhome
+# 如果 feeds 没有，需要你自行放入 diypath/adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git $DIYPATH/adguardhome
+
+echo "=== Step 3: Add ttyd ==="
+git clone --depth=1 https://github.com/tsl0922/ttyd.git $DIYPATH/ttyd
+
+echo "=== Step 4: Add custom themes ==="
+# 举例添加 luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git $DIYPATH/luci-theme-argon
+
+echo "=== Step 5: Fix dependencies ==="
+# 强制安装 QModem 依赖包
+cat >> $OPENWRT_DIR/feeds.conf.default <<EOL
+src-git qmodem https://github.com/FUjr/QModem.git;main
+EOL
+
+# 如果需要，可以在这里自动添加 QModem 的 Build/Host deps
+# sed 或 echo 修改 Makefile 或依赖的 kmod
+
+echo "=== Step 6: All DIY packages ready ==="
+ls -l $DIYPATH
